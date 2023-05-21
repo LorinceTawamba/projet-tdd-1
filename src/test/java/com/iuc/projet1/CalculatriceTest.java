acba4f9afc493b297d5e50ec78ef23509d8f1ff2
Lorince Tawamba <lorince.tawamba@gmail.com>
2023-05-21 06:43:22 -0400

    Update JUnit Dependencies, and create test methodes in CalculatriceTest.java

--- src/test/java/com/iuc/projet1/CalculatriceTest.java
+++ src/test/java/com/iuc/projet1/CalculatriceTest.java
@@ -1,4 +1,36 @@
 package com.iuc.projet1;
 
+import org.junit.jupiter.api.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CalculatriceTest {
+
+    @BeforeAll
+    public void setUp() {
+
+    }
+
+    @Test
+    @Order(1)
+    public void addition() {
+
+    }
+
+    @Test
+    @Order(2)
+    public void soustraction() {
+
+    }
+
+    @Test
+    @Order(3)
+    public void multiplication() {
+
+    }
+
+    @Test
+    @Order(4)
+    public void division() {
+
+    }
 }
