9c4f9900da4d6bfdee8ada8b5e6ef64d0932903f
Lorince Tawamba <lorince.tawamba@gmail.com>
2023-05-21 06:52:10 -0400

    Update methode addition

--- src/test/java/com/iuc/projet1/CalculatriceTest.java
+++ src/test/java/com/iuc/projet1/CalculatriceTest.java
@@ -2,18 +2,35 @@ package com.iuc.projet1;
 
 import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CalculatriceTest {
 
+    private double x;
+    private double y;
+    private double r;
+    private int number;
+
     @BeforeAll
     public void setUp() {
-
+        x=100*Math.random();
+        y=50*Math.random();
+        number=(int)(1000*Math.random());
     }
 
     @Test
     @Order(1)
     public void addition() {
-
+        System.out.println("===== TEST ADDITION =====");
+        r = x + y;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("r = " + r);
+        // addition(2,4) == 6
+        Calculatrice cal = new Calculatrice(x,y);
+        // 2, 4, 6
+        assertEquals(r, cal.addition());
     }
 
     @Test
